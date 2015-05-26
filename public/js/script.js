8b657f63e7154c2a3572bc2b304970ac5e3a2a73
ctrembath <claretrembath@ymail.com>
2015-05-26 16:08:06 +0100

    change

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,22 @@
 $(document).ready(function() {
+	  setTimeout(function() {
+    $('#fullpage').css("-webkit-filter", "blur(5px)")
+  }, 200);
+  setTimeout(function() {
+    $('#fullpage').css("-webkit-filter", "blur(4px)")
+  }, 400);
+  setTimeout(function() {
+    $('#fullpage').css("-webkit-filter", "blur(3px)")
+  }, 600);
+  setTimeout(function() {
+    $('#fullpage').css("-webkit-filter", "blur(2px)")
+  }, 800);
+  setTimeout(function() {
+    $('#fullpage').css("-webkit-filter", "blur(1px)")
+  }, 1000);
+   setTimeout(function() {
+    $('#fullpage').css("-webkit-filter", "blur(0px)")
+  }, 1200);
 
 	var hellos = setTimeout(function() {
 	  $("#hi").fadeIn(1500);
