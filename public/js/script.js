1c71f27863b9fb702f86e60254e5e8cbb2dcbc23
imarkwick <izzymarkwick@gmail.com>
2015-05-26 12:48:31 +0100

    top header

---
+++ public/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+	var hellos = setTimeout(function() {
+	  $("#hi").fadeIn(3000);
+	  $("#hi").fadeOut(100);
+	  $("#im").fadeIn(5000);
+	  $("#im").fadeOut(100);
+	  $("#clare").fadeIn(7000);
+	  1000
+	});
+
+});
